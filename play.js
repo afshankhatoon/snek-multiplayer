b410ad0125147032b36332d2faf5857b5231c732
afshankhatoon <afshankhatoon.job@gmail.com>
2020-11-05 03:35:18 +0000

    w02d3 client setup, connection module

--- play.js
+++ play.js
@@ -9,3 +9,21 @@ const game = new Game(new UserInterface(), new RemoteInterface())
 
 // Begin game
 game.start()
+
+//const net = require('net');
+const connect = require('./client');
+ /* const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  conn.setEncoding('utf8');
+  conn.on('data', (data) => {
+    console.log('Game Server says: ', data);
+  });
+  return conn;
+} */
+
+console.log('Connecting ...');
+connect();
+
